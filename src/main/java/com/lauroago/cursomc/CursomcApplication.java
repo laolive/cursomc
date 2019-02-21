eb48d7919e61fbc9645d11c148f5f91e6f60a2c8
Lauro Oliveira <lauro.ago@gmail.com>
2019-02-20 21:44:31 -0300

    Inclusão de registros automatica

--- src/main/java/com/lauroago/cursomc/CursomcApplication.java
+++ src/main/java/com/lauroago/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.lauroago.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.lauroago.cursomc.domain.Categoria;
+import com.lauroago.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Categoria cat1 = new Categoria (null, "Informatica");
+		Categoria cat2 = new Categoria (null, "Escritorio");
+		 
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
